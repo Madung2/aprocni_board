97083868b6419941f75d705bc7c0a2e7d1785da3
예슬_노트북\tulip <tuliphan91@gmail.com>
2022-09-28 20:51:45 +0900

    feat/ make user model adjusting abstractuser

--- aprocni_board/settings.py
+++ aprocni_board/settings.py
@@ -33,6 +33,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+AUTH_USER_MODEL = 'user.UserModel'
 
 # Application definition
 
@@ -43,6 +44,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "user",
+    "article",
 ]
 
 MIDDLEWARE = [
--- article/models.py
+++ article/models.py
@@ -1,3 +1,13 @@
+from ipaddress import ip_address
 from django.db import models
 
 # Create your models here.
+# class article_A():
+#     title=models.CharField("제목", max_length=80)
+#     author=models.ForeignKey()
+    # password=models.CharField("비밀번호")
+    # content=models.TextField("내용")
+    # ip_address=models.GenericIPAddressField()
+    # created_at=models.DateTimeField()
+    # updated_at=models.DateTimeField()
+    # board_A_id=models.IntegerField()
